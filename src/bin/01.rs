aff9cf78e2e35b17d2f1039e0d9b07ebbc346545
Mia Kiesman <n.kizz2711@gmail.com>
2024-12-01 21:56:57 -0500

    Day 1, part 2

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 advent_of_code::solution!(1);
 
 pub fn part_one(input: &str) -> Option<u32> {
@@ -21,7 +23,26 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let (left, right): (Vec<_>, Vec<_>) = input
+        .lines()
+        .map(|x| {
+            x.split_whitespace()
+                .map(|x| x.parse::<i32>().unwrap())
+                .collect::<Vec<_>>()
+        })
+        .map(|x| (x[0], x[1]))
+        .unzip();
+    let freq = right
+        .into_iter()
+        .fold(HashMap::<i32, i32>::new(), |mut m, x| {
+            *m.entry(x).or_default() += 1;
+            m
+        });
+    Some(
+        left.into_iter()
+            .map(|x| (*freq.get(&x).unwrap_or(&0) * x))
+            .sum::<i32>() as u32,
+    )
 }
 
 #[cfg(test)]
@@ -31,12 +52,12 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(11));
     }
 
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(31));
     }
 }
